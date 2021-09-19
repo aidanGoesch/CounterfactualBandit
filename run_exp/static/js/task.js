e5e8b6fce07aa279badf144c2b59a65f8de06315
noraharhen <noraharhen@gmail.com>
2021-09-19 11:00:32 -0400

    fixed task code and images

--- run_exp/static/js/task.js
+++ run_exp/static/js/task.js
@@ -78,7 +78,7 @@ let contextArray_ind = [0,1,2,3,4,5]
 contextArray_ind = randomize(contextArray_ind);
 let contextArray = []
 for (let i = 0; i < 6; i++) {
-  contextArray.push(contextArray_true[contextArray_ind])
+  contextArray.push(contextArray_true[contextArray_ind[i]])
 }
 contextArray.push(makeObject('run_exp/static/images/context_blank.png','blank'))
 contextArray.push(makeObject('run_exp/static/images/context_prac.png','prac'))
@@ -122,23 +122,24 @@ for (let i = 241; i < 260; i++) {
 }
 
 for (let i = 260; i < 265; i++) {
-  prac_probe_image.push(makeObject('run_exp/static/images/probes-'+i+'.png','probe-'+i))
+  prac_probe_images.push(makeObject('run_exp/static/images/probes-'+i+'.png','probe-'+i))
 }
 
 
 // randomize the array of images
-valid_probe_images_ind = randomize([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239]); // decision_ttrial --> proobe
-invalid_probe_images_ind = randomize([241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255, 256, 257, 258, 259]);
+let valid_probe_images_ind = randomize([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239]); // decision_ttrial --> proobe
+let invalid_probe_images_ind = randomize([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]);
 
 let valid_probe_images = [];
 let invalid_probe_images = [];
 
+
 for (let i = 0; i < 240; i++) {
-  valid_probe_images.push(valid_probe_images_true[valid_probe_images_ind])
+  valid_probe_images.push(valid_probe_images_true[valid_probe_images_ind[i]])
 }
 
 for (let i = 0; i < 19; i++) {
-  invalid_probe_images.push(invalid_probe_images_true[invalid_probe_images_ind])
+  invalid_probe_images.push(invalid_probe_images_true[invalid_probe_images_ind[i]])
 }
 
 
@@ -177,9 +178,9 @@ var probabilities = { 1: payout[0].map(x=>x*0.01),
   3: payout[2].map(x=>x*0.01)
 }
 
-pRew_Red = probabilities[1];
-pRew_White = probabilities[2];
-pRew_Black = probabilities[3];
+var pRew_Red = probabilities[1];
+var pRew_White = probabilities[2];
+var pRew_Black = probabilities[3];
 
 var response_key_dict = {
   49: '1',
