3a92f7ab489ea51176ed5de280eb021ea78094d7
noraharhen <noraharhen@gmail.com>
2022-04-14 00:40:10 -0700

    update database

--- data_analysis/create_database.py
+++ data_analysis/create_database.py
@@ -19,7 +19,7 @@ def make_sub_dict(data):
         sub_dict[var] = format_list(data,var,data_type)
     return sub_dict
 
-files = glob("../data/*2022-04-12_*csv") 
+files = glob("../data/*2022-04-12_*csv")  + glob("../data/*2022-04-13_*csv") 
 #files = glob("../data/*2021-11-02_*csv")
 db = dict()
 
