e56ae4d45729334500618582641ef7c6a82c37b5
RohinPalsule <rohin.palsule@gmail.com>
2026-03-09 13:09:12 -0700

    Update instructions.js

--- static/js/instructions.js
+++ static/js/instructions.js
@@ -59,7 +59,6 @@ var instruc1 = {
   on_finish: function(data) {
     data.trial_type = "instruc1";
     data.subject_id = subject_id;
-    data.participant_id = participant_id;
     data.age = age;
     data.gender = gender;
     data.prompt = welcome_txt;
